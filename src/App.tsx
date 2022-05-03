0c405dd4c3d8b478574e0808d41e83a516dada18
pianeta05 <pianeta05@hotmail.com>
2022-05-03 17:39:24 +0100

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,13 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let content : string = "Hello world";
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          {content}
         </p>
         <a
           className="App-link"
