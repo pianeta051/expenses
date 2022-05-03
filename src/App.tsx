cc1566d0e9f5890bb5f91bf1a3cb887727ba981a
pianeta05 <pianeta05@hotmail.com>
2022-05-03 18:22:34 +0100

    Clean project

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   const content : string = 'Hello world';
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          {content}
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <p>
+      {content}
+    </p>
   );
 }
 
